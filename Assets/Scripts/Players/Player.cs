14750d6c3c7b33f884894577e26398cafe031851
Ryan Nielson <ryan.nielson@gmail.com>
2015-04-18 01:21:00 -0300

    Added some bobbing.

---
+++ Assets/Scripts/Players/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using DG.Tweening;
+
+public class Player : MonoBehaviour 
+{
+    [SerializeField]
+    private float bobSpeed = 0.1f;
+
+    private KeyCode nextKey = KeyCode.A;
+
+    private float lastKeyTime = 0;
+
+	void Update () 
+    {
+        CheckKeys();
+	}
+
+    private void CheckKeys()
+    {
+        if (Input.GetKeyUp(nextKey))
+        {
+            if (nextKey == KeyCode.A)
+            {
+                transform.DORotate(new Vector3(0, 0, 45f), bobSpeed);
+                nextKey = KeyCode.D;
+            }
+            else if (nextKey == KeyCode.D)
+            {
+                transform.DORotate(new Vector3(0, 0, -45f), bobSpeed);
+                nextKey = KeyCode.A;
+            }
+        }
+    }
+}
